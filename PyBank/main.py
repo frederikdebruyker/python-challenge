0852a261e69aaf760cac4cf72564002ffa5fb88d
frederikdebruyker <46072905+frederikdebruyker@users.noreply.github.com>
2019-02-19 17:52:04 -0600

    Add existing file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,14 @@
 import csv
-with open('Resources/budget_data.csv', newline='') as csvfile:
-           filereader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-           for row in spamreader:
-                print(', '.join(row))
+
+#with open('C:/Users/Administrator/Google Drive/Rice Big Data Bootcamp/hw/hw3/Resources/budget_data.csv') as csv_file:    
+with open('C:/Users/Administrator/Google Drive/Rice Big Data Bootcamp/hw/hw3/Resources/election_data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'{row[0]} , {row[1]}')
+            line_count += 1
+        print(f'Processed {line_count} lines.')
